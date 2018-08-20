8f9293578e84f6498b20c7354bf90f4215a1a178
Justin Azoff <justin.azoff@gmail.com>
2018-08-20 17:44:29 -0400

    fix cli parsing

--- main.go
+++ main.go
@@ -65,26 +65,42 @@ func Filter(r *os.File, w io.Writer, filters []filter) (uint64, error) {
 
 func parseQuery(query []string) ([]filter, error) {
 	var filters []filter
-	for i, s := range query {
-		if i > 0 && query[i-1] == "body" {
-			filters = append(filters, filter(s))
+	for i, arg := range query {
+		if i%2 == 0 {
+			continue
 		}
-		if i > 0 && query[i-1] == "ip" {
-			ip, err := IPToFilter(s)
+		op := query[i-1]
+		if op == "body" {
+			filters = append(filters, filter(arg))
+		} else if op == "ip" {
+			ip, err := IPToFilter(arg)
 			if err != nil {
 				return filters, err
 			}
 			filters = append(filters, ip)
+		} else {
+			return filters, fmt.Errorf("Invalid filter %s", op)
 		}
 	}
 	return filters, nil
 }
 
+func usage() {
+	fmt.Fprintf(os.Stderr, "Usage: %s query < in.pcap > out.pcap\nWhere query is 'body foo' or 'ip 1.2.3.4/32\n", os.Args[0])
+	os.Exit(1)
+}
 func main() {
+	if len(os.Args) < 3 {
+		usage()
+	}
 	fs, err := parseQuery(os.Args[1:])
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "invalid query %v", os.Args[1:])
-		os.Exit(1)
+		fmt.Fprintf(os.Stderr, "invalid query: %v\n", err)
+		usage()
+	}
+	if len(fs) == 0 {
+		fmt.Fprintf(os.Stderr, "Missing query\n")
+		usage()
 	}
 
 	pkts, err := Filter(os.Stdin, os.Stdout, fs)
