a5ad7e928f72fd63292ebf29b7bfe5f31f688060
Justin Azoff <justin.azoff@gmail.com>
2018-08-21 00:38:14 +0000

    reorg

--- pcap_indexer/cmd/index_pcap/main.go
+++ pcap_indexer/cmd/index_pcap/main.go
@@ -1,45 +1,11 @@
 package main
 
 import (
-	"io"
 	"log"
 	"os"
 
 	"github.com/JustinAzoff/pcapfilter/pcap_indexer"
-	"github.com/google/gopacket/pcapgo"
 )
-
-func IndexPCAP(r io.Reader, w io.Writer, minOffset int) error {
-	pr, err := pcapgo.NewReader(r)
-	if err != nil {
-		return err
-	}
-
-	ow := pcap_indexer.NewIndexWriter(w)
-
-	lastPos := 0
-	pos := 24
-	pkt := 1
-	for {
-		_, captureInfo, err := pr.ReadPacketData()
-		if err == io.EOF {
-			break
-		}
-		if err != nil {
-			return err
-		}
-		pkt++
-		pos += 16 + captureInfo.CaptureLength
-		//log.Printf("packet: %d, pos: %d, len: %d", pkt, pos, captureInfo.CaptureLength)
-		if pos-lastPos > minOffset {
-			ow.WriteOffset(pos)
-			//log.Printf("Wrote offset: %d", pos)
-			lastPos = pos
-		}
-	}
-	return nil
-
-}
 
 func main() {
 	f := os.Args[1]
@@ -57,7 +23,7 @@ func main() {
 	}
 	defer w.Close()
 
-	err = IndexPCAP(r, w, 128*1024)
+	err = pcap_indexer.IndexPCAP(r, w, 128*1024)
 	if err != nil {
 		log.Fatal(err)
 	}
---
+++ pcap_indexer/indexpcap.go
@@ -0,0 +1,42 @@
+package pcap_indexer
+
+import (
+	"io"
+	"log"
+
+	"github.com/google/gopacket/pcapgo"
+)
+
+func IndexPCAP(r io.Reader, w io.Writer, maxOffset int) error {
+	pr, err := pcapgo.NewReader(r)
+	if err != nil {
+		return err
+	}
+
+	ow := NewIndexWriter(w)
+
+	lastPos := 0
+	pos := 24
+	nextPos := 0
+	pkt := 1
+	for {
+		_, captureInfo, err := pr.ReadPacketData()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			return err
+		}
+		pkt++
+		nextPos = pos + 16 + captureInfo.CaptureLength
+		//log.Printf("packet: %d, pos: %d, len: %d", pkt, pos, captureInfo.CaptureLength)
+		if nextPos-lastPos > maxOffset {
+			ow.WriteOffset(pos)
+			log.Printf("Wrote offset: %d", pos)
+			lastPos = pos
+		}
+		pos = nextPos
+	}
+	return nil
+
+}
